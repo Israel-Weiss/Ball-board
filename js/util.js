de5f241fb9b7c229105cfe1b59ac911e387f9084
Israel Weiss <93487387+Israel-Weiss@users.noreply.github.com>
2022-08-01 23:21:40 +0300

    Add files via upload

---
+++ js/util.js
@@ -0,0 +1,37 @@
+'use strict'
+
+function createMat(ROWS, COLS) {
+    var mat = []
+    for (var i = 0; i < ROWS; i++) {
+        var row = []
+        for (var j = 0; j < COLS; j++) {
+            row.push('')
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function handleKey(event) {
+	var i = gGamerPos.i
+	var j = gGamerPos.j
+
+	switch (event.key) {
+		case 'ArrowLeft':
+			moveTo(i, j - 1)
+			break
+		case 'ArrowRight':
+			moveTo(i, j + 1)
+			break
+		case 'ArrowUp':
+			moveTo(i - 1, j)
+			break
+		case 'ArrowDown':
+			moveTo(i + 1, j)
+			break
+	}
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
